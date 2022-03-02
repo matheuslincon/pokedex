f6efeeab824a74b7afec711acf0c3ef7fa02f217
Matheus Andrade <matheuslincon50@gmail.com>
2022-03-02 09:45:53 -0300

    used useState on all pokemons and show more button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 function App() {
+
+  const [ allPokemons, setAllPokemons ] = useState([]);
+  const [ showMore, setShowMore ] = useState('https://pokeapi.co/api/v2/pokemon?limit=100')
+
   return (
     <div className="app-container">
       <h1>Pokedex</h1>
       <div className="pokemon-container">
 
       </div>
+      <button className="show-more">Show More !</button>
     </div>
   );
 }
